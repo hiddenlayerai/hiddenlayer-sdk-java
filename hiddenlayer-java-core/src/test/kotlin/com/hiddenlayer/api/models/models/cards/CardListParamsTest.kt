3bbe5bab834058deb1aec5f8516f3f0de7f98ce9
Ben Lucas <blucas@hiddenlayer.com>
2025-09-30 22:46:18 +0000

    fix test

--- hiddenlayer-java-core/src/test/kotlin/com/hiddenlayer/api/models/models/cards/CardListParamsTest.kt
+++ hiddenlayer-java-core/src/test/kotlin/com/hiddenlayer/api/models/models/cards/CardListParamsTest.kt
@@ -103,7 +103,7 @@ internal class CardListParamsTest {
         assertThat(queryParams)
             .isEqualTo(
                 QueryParams.builder()
-                    .put("aidr_severity", listOf("SAFE").joinToString(","))
+                    .put("aidr_severity", listOf(CardListParams.ModscanSeverity.ModelCardScanSafeUnsafe.SAFE))
                     .put("aidr_status", "ENABLED")
                     .put("limit", "50")
                     .put("model_created[gte]", "2019-12-27T18:11:19.117Z")
